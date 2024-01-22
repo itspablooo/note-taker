8bcc65a7cd6fe9cb8c33501927c8d67316df9d63
Pablo Rivera <pablomrivera@outlook.com>
2024-01-21 22:00:51 -0700

    add routes and middleware

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // middleware to tell express to look for html files in the public directory. 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// setting up routes
+// root '/' will send index.html (static file)
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+// '/notes' will send notes.html (static file)
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')))
 
 // make server listen for incoming HTTP request on specified port, then console log confirmation that the server is running. 
-app.listen(PORT, () => console.log(`The server is running on https://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`The server is running on http://localhost:${PORT}/`))
