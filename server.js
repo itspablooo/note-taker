44551aa84bf43da6596b91be74e2cdbe89795496
Pablo Rivera <pablomrivera@outlook.com>
2024-02-03 20:13:58 -0700

    add post route

--- server.js
+++ server.js
@@ -14,11 +14,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// setting up routes
-// root '/' will send index.html (static file)
+
+// static files 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-// '/notes' will send notes.html (static file)
+
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')))
+
+//api get route for notes in json format 
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', function(err, data) {
+        res.json(JSON.parse(data));
+    });
+});
+
+// post routes 
+app.post('/api/notes', (req, res) => {
+    const {title, text} = req.body;
+
+    if (title && text) {
+        const newNote = {title, text};
+
+        const newNoteString = JSON.stringify(newNote);
+
+        fs.writeFile('./db/db.json', newNoteString, (err) =>
+         err ? console.error(err) : console.log('New note has been added')
+        );
+    };
+});
+
 
 // make server listen for incoming HTTP request on specified port, then console log confirmation that the server is running. 
 app.listen(PORT, () => console.log(`The server is running on http://localhost:${PORT}/`))
